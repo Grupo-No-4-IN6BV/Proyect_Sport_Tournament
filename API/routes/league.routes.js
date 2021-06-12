a4dd76e3fe21f4647e5297cd84ea3e3947eed14a
Spencer Ortiz <sortiz-2019191@kinal.edu.gt>
2021-06-12 16:32:56 -0600

    Rutas y Modelo de liga

--- API/routes/league.routes.js
+++ API/routes/league.routes.js
@@ -9,3 +9,5 @@ api.put('/saveLeague', [mdAuth.ensureAuth, mdAuth.ensureAuthAdmin], leagueContro
 api.put('/updateLeague/:id', [mdAuth.ensureAuth, mdAuth.ensureAuthAdmin], leagueController.updateLeague);
 api.get('/removeLeague/:id', [mdAuth.ensureAuth, mdAuth.ensureAuthAdmin], leagueController.removeLeague);
 api.post('/search', mdAuth.ensureAuth, leagueController.searchLeague);
+
+module.exports = api;
